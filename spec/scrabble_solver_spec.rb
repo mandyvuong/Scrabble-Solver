a16c37eb1aafa629cabf499be1a05dce9ad23f05
amanda vuong <mandyvuong@gmail.com>
2021-12-04 22:47:51 +0000

    returns score 0 as there is no word taken as an argument

--- spec/scrabble_solver_spec.rb
+++ spec/scrabble_solver_spec.rb
@@ -1,1 +1,8 @@
 require 'scrabble_solver'
+
+describe Scrabble do
+  it 'returns score 0 as there is no word taken as an argument' do
+    scrabble = Scrabble.new('')
+    expect(scrabble.score).to eq 0
+  end
+end
