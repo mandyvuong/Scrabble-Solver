60908af05ff669892e470fbc78250fcaa20bb719
amanda vuong <mandyvuong@gmail.com>
2021-12-04 22:49:07 +0000

    returns score 0 since the word argument is format

--- spec/scrabble_solver_spec.rb
+++ spec/scrabble_solver_spec.rb
@@ -5,4 +5,8 @@ describe Scrabble do
     scrabble = Scrabble.new('')
     expect(scrabble.score).to eq 0
   end
+  it 'returns score 0 since the word argument is format' do
+    scrabble = Scrabble.new(" \t\n")
+    expect(scrabble.score).to eq 0
+  end
 end
