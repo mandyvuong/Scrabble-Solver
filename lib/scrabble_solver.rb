30736b869d6fa723868c1c09d47758ced2bcc651
amanda vuong <mandyvuong@gmail.com>
2021-12-05 13:26:58 +0000

    implemented hash

--- lib/scrabble_solver.rb
+++ lib/scrabble_solver.rb
@@ -4,12 +4,17 @@ class Scrabble
   end
 
   def score
+    letter_value = { 
+      'A' => 1
+     } 
+    
     sum = 0
      if @word.nil? 
       sum = 0
-     else
-      a = 1 
-      sum = @word.downcase.count('a') * a
+     else 
+      letter_value.each { |letter, value|
+      sum += @word.upcase.scan(letter).count * value
+      }
      end
     sum
   end
